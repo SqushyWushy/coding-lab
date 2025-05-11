025d4eb78dd698407ae5d76a73c43b00320fb45a
Jr Hector Gonzalez <142999471+SqushyWushy@users.noreply.github.com>
2025-05-11 12:26:48 -0500

    update index and script

--- html/hello-website/script.js
+++ html/hello-website/script.js
@@ -1,3 +1,24 @@
 function handleClick() {
     alert("You clicked the button!");
 }
+
+function submitEmail() {
+    const emailInput = document.getElementById('email-input');
+    const responseMessage = document.getElementById('response-message');
+
+    fetch('/submit-email',  {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ email: emailInput.value })
+    })
+    .then(res => res.json())
+    .then(data => {
+        responseMessage.textContent = data.message;
+        emailInput.value = '';
+    })
+    .catch(() => {
+        responseMessage.textContent = "Something went wrong.";
+    });
+}
