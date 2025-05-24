8288e81707ab226b65e859dcd3e6974e4a310b8c
Jr Hector Gonzalez <142999471+SqushyWushy@users.noreply.github.com>
2025-05-24 13:28:21 -0500

    Update script.js

--- html/hello-website/script.js
+++ html/hello-website/script.js
@@ -1,24 +1,3 @@
 function handleClick() {
-    alert("You clicked the button!");
-}
-
-function submitEmail() {
-    const emailInput = document.getElementById('email-input');
-    const responseMessage = document.getElementById('response-message');
-
-    fetch('/submit-email',  {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({ email: emailInput.value })
-    })
-    .then(res => res.json())
-    .then(data => {
-        responseMessage.textContent = data.message;
-        emailInput.value = '';
-    })
-    .catch(() => {
-        responseMessage.textContent = "Something went wrong.";
-    });
+  alert("You clicked the button!");
 }
